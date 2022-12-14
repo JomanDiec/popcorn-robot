8d154b5b479fdd374c5725506dea590e9109f77b
JomanDiec <jobiochem@yahoo.com>
2022-12-14 05:53:02 +0000

    class 12-13-22

--- haunted_mansion/models.py
+++ haunted_mansion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class GameState(models.Model):
+    new_game = models.BooleanField(default=True)
+    health = models.IntegerField(default=5)
+    bedroom_key = models.BooleanField(default=False)
+    front_key = models.BooleanField(default=False)
+    jack_found = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f" is it a new game: {self.new_game}, health is currently: {self.health}, is the bedroom key found: {self.bedroom_key}, is the front door key found {self.front_key}, has jack been found: {self.jack_found}"
--- haunted_mansion/views.py
+++ haunted_mansion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from haunted_mansion.models import *
 
 # Create your views here.
 def index(request):
@@ -7,6 +8,12 @@ def index(request):
 
 def haunted_mansion(request):
 
-    # return HttpResponse('haunted mansion')
+    game_state = GameState.objects.get(id=1)
+    new_game = game_state.new_game
+    health = game_state.health
+    bedroom_key = game_state.bedroom_key
+    front_key = game_state.front_key
+    jack_found = game_state.jack_found
 
-    return render(request,'haunted_mansion.html')
+    context = {'new_game' : new_game, 'health' : health, 'bedroom_key' : bedroom_key, 'front_key' : front_key, 'jack_found' : jack_found}
+    return render(request,'ajax_haunted_mansion.html', context)
