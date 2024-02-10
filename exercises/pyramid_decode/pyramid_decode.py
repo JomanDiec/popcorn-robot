fa95857ca6710f14a9f7f55e163aea7f9a01519d
Joman Diec <jobiochem@yahoo.com>
2024-02-09 16:46:30 -0800

    pyramid_decode job app material added

---
+++ exercises/pyramid_decode/pyramid_decode.py
@@ -0,0 +1,50 @@
+# The primary tasks needed to perform the function are broken down into smaller, easily readable sub-functions in B. This makes it easier for other coders to examine and make modifications to the code. Also, this was the only choice where the code was not repetitive which is usually preferable if you want to find a general solution to a problem as opposed to a solution that only works for that specific example.
+
+# A is mathematically incorrect as n can have factors that are greater than the square root of n which this function would miss. The for loop in B is redundant; if the function already has a method that finds all factors in the range shown then by definition you just need to check if the list the function method returns is empty or not to check if it is prime. C is the only one that would both work and has no redundant code (though it could be made slightly more efficient by restricting the range to 1/2n as no integer factor for n can be greater than 1/2n).
+
+# A is probably more appropriate for general audiences as we should not be encouraging unethical behavior by making such tasks easier for people to achieve. However, as in the case of B, knowing how to do it is not in and of itself unethical and it could be argued that there is value in understanding how such tasks can be achieved e.g in the interest of cybersecurity, and by including the preface in response B before the answer it acknowledges that. Depending on the audience that the AI is being designed for either response has its merits.
+
+# While as a general rule, I'd err to the more detailed responses over shorter ones, this is again a matter of the audience this AI is being designed for. Response A would be better for someone still learning to code who would benefit from understanding how the problem is broken down and what is done at each step of the process. Response B would probably be preferable for more experienced coders who are looking to solve the problem but would just skim over or be annoyed by having to read details they already understand. In an ideal world, someone who would initially benefit from response A would eventually grow in their understanding to eventually prefer B.
+
+
+
+decode_data = []
+new_order = []
+
+with open('coding_qual_input.txt', 'r') as file: 
+  for line in file:
+    data = line.split()
+    new_order.append(data)
+
+def sortFirst(val): 
+    return int(val[0])
+
+new_order.sort(key=sortFirst)
+
+index = 0
+level = 1
+
+for data in new_order:
+   if (new_order.index(data)) == index:
+       decode_data.append(data)
+       level += 1
+       index += level
+
+string = ''
+
+for data in decode_data:
+   string += ' ' + data[1]
+
+print(string)
+
+# Explaination:
+# This function works by first extracting the data from the coding_qual_input.txt file into the array 'new_order' then sorting it by the first item of each list cast as an integer.
+# It then uses a for loop to iterate through 'new_order' by checking if the current item's index is equal to the 'index' variable. If it is, then that item is added to the array 'decode_data' and the 'level' and 'index' variables are updated before moving on to the next item.
+# Finally, it uses one last for loop to iterate through the complete 'decode_data' array, adding a space and the word in each item i.e. data[1], to the 'string' variable which is what gets returned by the function.
+
+
+# I'm a biologist by education (MS in biotechnology) and have been doing research work at various labs for most of my life. In recent years I have been taking classes in full-stack web development through a mentorship program (not a boot camp as my mentor likes to emphasize!) followed by some paid, part-time web development work during and after graduation. I initially did this to complement my existing skills as a biologist so I could work in a field like bioinformatics but I'd be open to a pure developer job if the right opportunity came along.
+
+# The questions and code challenge were fun! I can easily see myself spending all day working on coding challenges like these and being able to get paid for it would be awesome! If you would like to see some of the coding challenges I've completed please check my Code Wars page.
+# https://www.codewars.com/users/JomanDiec/completed
+
